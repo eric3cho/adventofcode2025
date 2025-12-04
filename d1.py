04e674e475b265659695c02686bde5ee9db3f268
eric_cho <eric3cho@gmail.com>
2025-12-03 20:15:57 -0800

    day1

--- d1.py
+++ d1.py
@@ -4,11 +4,22 @@ import numpy as np
 # 0 - 99
 def secret_entrance():
     file = open('day1.txt')
-    pword = 0
+    password = 0
     curr = 50
+    prev = 0
     for line in file:
-        if line[0] == "L":
-
+        sign = line[0]
+        val = int(line[1:])
+        if sign == 'R':
+            curr += val
         else:
-
+            curr -= val
+        is_zero = (abs(curr) % 100 == 0) or (curr == 0)
+        if is_zero and sign == 'L':
+            password += 1
+        dial = curr // 100
+        if dial != prev:
+            password += abs(dial-prev)
+        prev = dial
+    print(password)
 
--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import d1
 
 # day1
 d1.secret_entrance()
+# day2
+
 
 
 
